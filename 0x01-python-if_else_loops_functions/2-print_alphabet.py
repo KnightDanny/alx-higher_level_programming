fbfcaa3c55351bd45829fb727657cb9e1105d32e
Danny <daniel.habtamut@aau.edu.et>
2023-11-30 04:09:44 +0300

    Even higher_0x01 edit

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to print the ASCII alphabet in lowercase
-# demonstrates how to use a loop to affect program output
-#
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
-# -----------------------------------------------------------
+import random
 
-# For each ascii_code in range sequence
-for ascii_code in range(97, 123):
+number = random.randint(-10000, 10000)
 
-    # Print out the character format of the ascii_code
-    print("{:c}".format(ascii_code), end='')
+if number > 0:
+    print(number, "is positive")
+elif number == 0:
+    print(number, "is zero")
+else:
+    print(number, "is negative")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,23 +1,10 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to print the smallest, unique combinations of two digits
-# demonstrates how to use loops and an if condition to affect program output
-#
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
-# -----------------------------------------------------------
-
-# Digits are 0 ... 9
-# Loop variables to increment:  first_digit, second_digit
-# For smallest combination first_digit < second_digit
-# For unique combination first_digit != second_digit
-# For smallest unique combination !(second_digit <= first_digit)
-
-for first_digit in range(0, 8):
-    for second_digit in range(0, 10):
-        if not (second_digit <= first_digit):
-            print("{:d}{:d}".format(first_digit, second_digit), end=", ")
-
-# Print last values for first_digit and second_digit
-print("89")
+for a in range(0, 10):
+    for b in range(1, 10):
+        if a == b or a > b:
+            continue
+        else:
+            if (a == 8):
+                print("89")
+            else:
+                print("{}{}".format(a, b), end=", ")
